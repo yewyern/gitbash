4d915a8ea89759d9d39a3133945cb1edd3cc963d
mayfool <691573424@qq.com>
2023-04-15 22:35:04 +0800

    git,task抽取到公共脚本

--- util.sh
+++ util.sh
@@ -1,3 +1,6 @@
+SUCCESS=0
+FAILED=1
+
 function success_log() {
   echo -e "\033[32m $* \033[0m"
 }
@@ -10,10 +13,37 @@ function trim() {
     echo "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
 }
 
+function get_continue() {
+  echo "$@"
+  # read不能在管道里使用
+  read toContinue
+  if [[ "Y" == "$toContinue" || "y" == "$toContinue" ]]; then
+    return $SUCCESS
+  fi
+  return $FAILED
+}
+
+# get_value_by_key <filename> <key> <key_index> <value_index>
 function get_value_by_key() {
     filename=$1
     key=$2
-    sep=$3
-    arr=(`grep "^$key$sep" $filename`)
-    echo ${arr[1]}
+    key_index=$3
+    value_index=$4
+    # grep 出符合条件的多行数据
+    lines=`grep "$key" $filename`
+    # 遍历进行匹配
+    for i in "${!lines[@]}";
+    do
+        line=${lines[$i]}
+        # 处理换行符
+        line=`echo $line | tr --delete '\n'`
+        line=`echo $line | tr --delete '\r'`
+        # 根据空格或tab分割字符串
+        arr=($line)
+        if [ "${arr[$key_index]}" == "$key" ]; then
+            echo ${arr[$value_index]}
+            return $SUCCESS
+        fi
+    done
+    return $FAILED
 }
