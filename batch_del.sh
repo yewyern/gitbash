f4d4c1f72bd496a58bbc567adcb16bf385938b2d
mayfool <691573424@qq.com>
2023-04-16 09:55:51 +0800

    重命名以及格式化

--- batch_del.sh
+++ batch_del.sh
@@ -6,24 +6,24 @@
 # @date: 2020-12-08
 
 function success_log() {
-  echo -e "\033[32m $* \033[0m"
+    echo -e "\033[32m $* \033[0m"
 }
 
 function error_log() {
-  echo -e "\033[31m $* \033[0m"
+    echo -e "\033[31m $* \033[0m"
 }
 
 if [ $# -lt 1 ]; then
-  error_log "Usage: batchdel.sh dirname"
-  error_log "example: batchdel.sh target"
-  exit 1
+    error_log "Usage: batchdel.sh dirname"
+    error_log "example: batchdel.sh target"
+    exit 1
 fi
 
 dirname=$1
-find . -name $dirname -type d > batchdel.tmp
+find . -name "$dirname" -type d >batchdel.tmp
 # 遍历文件，每次处理一行
-while read line || [[ -n $line ]]; do
-  success_log "当前行：$line"
-  rm -rf $line
-done < batchdel.tmp
+while read -r line || [[ -n $line ]]; do
+    success_log "删除：$line"
+    rm -rf "$line"
+done <batchdel.tmp
 rm -f batchdel.tmp
